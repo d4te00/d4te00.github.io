2c86b30b59d1f27ccb7cbbb89357cd45cee2e969
d4te00 <68460330+d4te00@users.noreply.github.com>
2024-01-12 18:56:27 +0530

    just some fooling around

--- speacialmessage.github.io/messagescript.js
+++
@@ -1,5 +0,0 @@
-const msg = document.querySelector('.msg');
-const urlpara = new URLSearchParams(window.location.search);
-const name = urlpara.get('name');
-
-msg.innerHTML = '<h1> Hey'+name+'</h1>';
